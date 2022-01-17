f84283f4f74f1dafb764e451af1104151729e971
BaklajaH-1337 <97811987+BaklajaH-1337@users.noreply.github.com>
2022-01-17 22:32:27 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const slides = document.querySelectorAll('.slide')
 
 for (const slide of slides) {
-    slide.addEventListener('mouseenter', () =>{
+    slide.addEventListener('mouseenter'||'click', () =>{
         clearActiveClasses()
         
         slide.classList.add('active')
     })
 }
+
 function clearActiveClasses() {
     slides.forEach((slide) => {
         slide.classList.remove('active')
